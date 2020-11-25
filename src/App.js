70112f41035886573f23dee7024eb5eb7623f6ca
GalicParmesanWings <Fenglinjoyyy@gmail.com>
2020-11-25 13:29:58 -0800

    added checkout page, checkout item component and updated cart to use selector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.conponent';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.util';
 import {setCurrentUser} from './redux/user/user.action';
+import {selectCurrentUser} from './redux/user/user.selectors';
 
 import './App.css';
 
@@ -45,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component = {HomePage} />
           <Route path='/shop' component = {ShopPage} />
+          <Route exact path='/checkout' component = {CheckoutPage} />
           <Route exact path='/signin' render = {() => 
           this.props.currentUser ? 
           (<Redirect to = '/' />) : 
@@ -55,8 +59,8 @@ class App extends React.Component {
       );
   }
 }
-const mapStateToProps = ({user}) =>({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 
