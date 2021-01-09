13b0ebbb35f196f0e357fe24123703ec83d755da
Galic-parmesan-wings <Fenglinjoyyy@gmail.com>
2021-01-08 21:54:44 -0800

    test2

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,7 +1,7 @@
 const INITIAL_STATE = {
     sections: [
         {
-          title: 'SOCKS',
+          title: 'hets',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
           linkUrl: 'shop/hats'
